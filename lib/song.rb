bd22a28efe8951b0a362cc81490d31ba94d3bbd9
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-22 10:56:00 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,18 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class Song
 
 
   def self.table_name
     self.to_s.downcase.pluralize
+
   end
+
+
+
+
 
   def self.column_names
     DB[:conn].results_as_hash = true
@@ -19,7 +25,12 @@ class Song
       column_names << row["name"]
     end
     column_names.compact
+    binding.pry
   end
+
+
+
+
 
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
@@ -31,15 +42,28 @@ class Song
     end
   end
 
+
+
+
+
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+
   end
+
+
+
+
 
   def table_name_for_insert
     self.class.table_name
   end
+
+
+
+
 
   def values_for_insert
     values = []
@@ -47,18 +71,31 @@ class Song
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
     values.join(", ")
+
   end
+
+
+
+
 
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
+
+
+
+
 
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
 
+
+
+
+
 end
 
-
-
+la_camisa_negra = Song.new({name: "Juanes", album: "la camisa negra"})
+la_camisa_negra.save
